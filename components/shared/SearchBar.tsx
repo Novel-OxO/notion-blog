30c9285134ebb42bd62bdd011c429a9c2e9304c6
Novel-OxO <novel0x0@gmail.com>
2025-10-08 07:13:56 +0900

    fix: 불필요한 css 제거

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -3,11 +3,11 @@ import { Input } from "../ui/input";
 
 export default function SearchBar() {
   return (
-    <div className="relative hologram-focus">
+    <div className="relative">
       <Input
         type="text"
         placeholder="검색어를 입력하세요..."
-        className="w-full bg-transparent border-gray-400 text-gray-50 placeholder:text-gray-400 rounded-lg  focus-visible:border-primary-900 focus-visible:ring-primary-900"
+        className="w-full bg-transparent border-gray-400 text-gray-50 rounded-lg placeholder:text-gray-400   focus-visible:border-primary-900 focus-visible:ring-primary-900"
       />
       <Search className="w-4 h-4 text-primary-900 absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none" />
     </div>
