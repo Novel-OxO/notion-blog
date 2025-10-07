6325a6e4fbbe59c444b8743cd4af7679a8df8a25
Novel-OxO <novel0x0@gmail.com>
2025-10-07 17:38:27 +0900

    add: notion data 조회

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { getPublicPosts } from "@/lib/notion";
 
-export default function Home() {
+// ISR 60초마다 업데이트
+export const revalidate = 60;
+
+export default async function Home() {
+  const { posts, hasMore, nextCursor } = await getPublicPosts();
+  console.log(posts);
+
   return (
     <div>
-      <Button>Click med </Button>
       <div className="bg-primary-900 text-white">진한 보라색 배경</div>
       <div className="bg-sub-green">초록색 배경</div>
       <div className="hologram w-64 h-64 rounded-full"></div>
